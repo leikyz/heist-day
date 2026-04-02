6ee02a55a19a48c23c3514fdc5350375ff49ba45
Leikyz <leikyz17@outlook.com>
2026-04-02 16:45:04 +0200

    feat: client bat added

--- Run/Client1.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set GAME_EXE="C:\Users\leiky\Desktop\Heist Day\HeistDay\Build\Windows\FinalGame.exe"
-
-start "" %GAME_EXE% -log -AUTH_LOGIN=DevUser -windowed -ResX=1280 -ResY=720
-
-exit
